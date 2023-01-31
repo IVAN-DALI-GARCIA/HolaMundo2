aef8c592f3dd89015eca883928d451551fdb9e11
DALIJORDAN <81706583+IVAN-DALI-GARCIA@users.noreply.github.com>
2023-01-30 23:30:41 -0600

    Actualiacion
    Esta es una nueva actualización de este proyecto.

--- Assets/holaMundo2.cs
+++ Assets/holaMundo2.cs
@@ -7,8 +7,8 @@ public class holaMundo2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hola mundo");
-        
+     print("hola mundo");
+
     }
 
     // Update is called once per frame
